cfc4b73176d29538a7c528673ab5dc7ed8e99a40
Kaprishtein <qwerty27101999@gmail.com>
2026-07-08 20:20:51 +0300

    fix ui tabs

--- components/pages/CompanyPage.tsx
+++ components/pages/CompanyPage.tsx
@@ -5,13 +5,9 @@ import { useState } from "react";
 import { CompanyDataset } from "@/lib/company/companyTypes";
 import { Employee } from "@/lib/types";
 
-import { useTable } from "@/lib/table/useTable";
-
 import { companyColumns, CompanyTable } from "@/components/company/CompanyTable";
 import { CompanyHeader } from "@/components/company/CompanyHeader";
 import { EmployeeDrawer } from "@/components/company/EmployeeDrawer";
-
-import { TableFilters } from "@/components/tables/TableFilters";
 
 interface Props {
 
@@ -33,14 +29,6 @@ export function CompanyPage({
 
   ] = useState<Employee | null>(null);
 
-  const table = useTable(
-
-    company.employees,
-
-    companyColumns
-
-  );
-
   return (
 
     <>
@@ -57,39 +45,9 @@ export function CompanyPage({
 
       />
 
-      
-
-      <TableFilters
-
-        search={table.search}
-
-        onSearchChange={table.setSearch}
-
-        sortField={table.sortField}
-
-        onSortFieldChange={table.setSortField}
-
-        ascending={table.ascending}
-
-        onAscendingChange={table.setAscending}
-
-        sortOptions={companyColumns
-
-          .filter(c => c.sortable)
-
-          .map(c => ({
-
-            value: String(c.id),
-
-            label: c.title,
-
-          }))}
-
-      />
-
       <CompanyTable
 
-        employees={table.rows}
+        employees={company.employees}
 
         selectedEmployee={selectedEmployee}
 
